9315e18a9cfaf63583499c21d791415f692faf66
AMS777 <alberto@oscuro.es>
2021-05-18 20:18:38 +0200

    Split file table toolbar into its own component

--- src/components/fileTable/FileTable.tsx
+++ src/components/fileTable/FileTable.tsx
@@ -1,42 +1,22 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 
-import { File, FileStatus } from '../../types';
+import { File } from '../../types';
 import { filesFixture } from '../../fixtures';
 
+import FileTableToolbar from './FileTableToolbar';
 import FileTableHead from './FileTableHead';
 import FileTableBody from './FileTableBody';
-
-import DownloadIcon from '../../images/download.svg';
 
 import './styles.scss';
 
 function FileTable() {
   const [files, setFiles] = useState<File[]>([]);
-  const [isSelectAllChecked, setIsSelectAllChecked] = useState(false);
   const [selectedFiles, setSelectedFiles] = useState(new Set<File>());
 
   useEffect(() => {
     setFiles(filesFixture);
   }, []);
-
-  useEffect(() => {
-    const selectAllCheckbox: HTMLInputElement = document.querySelector(
-      '#file-table .select-all-checkbox',
-    )!;
-    selectAllCheckbox.indeterminate = false;
-
-    if (selectedFiles.size === 0) {
-      setIsSelectAllChecked(false);
-      return;
-    }
-    if (selectedFiles.size === files.length) {
-      setIsSelectAllChecked(true);
-      return;
-    }
-
-    selectAllCheckbox.indeterminate = true;
-  }, [selectedFiles, files]);
 
   const handleSelectFile = (file: File) => {
     if (selectedFiles.has(file)) {
@@ -47,35 +27,9 @@ function FileTable() {
     setSelectedFiles(new Set<File>(selectedFiles));
   };
 
-  const handleDownloadFiles = () => {
-    const selectedFilesWithAvailableStatus: File[] = Array.from(selectedFiles).filter(
-      (file: File) => file.status === FileStatus.Available,
-    );
-
-    if (selectedFilesWithAvailableStatus.length > 0) {
-      alert(
-        selectedFilesWithAvailableStatus.reduce(
-          (message: string, file: File) => `${message}- ${file.device}: ${file.path}\n`,
-          'Available files to download:\n\n',
-        ),
-      );
-    } else {
-      alert('There are no selected available files to download.');
-    }
-  };
-
   return (
     <div id="file-table" data-test="homepage-border">
-      <header>
-        <input type="checkbox" className="select-all-checkbox" checked={isSelectAllChecked} />
-        <span className="selected-files-count">
-          {selectedFiles.size === 0 ? 'None Selected' : `Selected ${selectedFiles.size}`}
-        </span>
-        <button onClick={handleDownloadFiles}>
-          <img src={DownloadIcon} alt="icon" />
-          Download Selected
-        </button>
-      </header>
+      <FileTableToolbar files={files} selectedFiles={selectedFiles} />
       <table data-test="file-table">
         <FileTableHead />
         <FileTableBody files={files} handleSelectFile={handleSelectFile} />
---
+++ src/components/fileTable/FileTableToolbar.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+import { File, FileStatus } from '../../types';
+
+import DownloadIcon from '../../images/download.svg';
+
+import './styles.scss';
+
+interface FileTableToolbaryProps {
+  files: File[];
+  selectedFiles: Set<File>;
+}
+
+function FileTableToolbar({ files, selectedFiles }: FileTableToolbaryProps) {
+  const [isSelectAllChecked, setIsSelectAllChecked] = useState(false);
+
+  useEffect(() => {
+    const selectAllCheckbox: HTMLInputElement = document.querySelector(
+      '#file-table .select-all-checkbox',
+    )!;
+    selectAllCheckbox.indeterminate = false;
+
+    if (selectedFiles.size === 0) {
+      setIsSelectAllChecked(false);
+      return;
+    }
+    if (selectedFiles.size === files.length) {
+      setIsSelectAllChecked(true);
+      return;
+    }
+
+    selectAllCheckbox.indeterminate = true;
+  }, [selectedFiles, files]);
+
+  const handleDownloadFiles = () => {
+    const selectedFilesWithAvailableStatus: File[] = Array.from(selectedFiles).filter(
+      (file: File) => file.status === FileStatus.Available,
+    );
+
+    if (selectedFilesWithAvailableStatus.length > 0) {
+      alert(
+        selectedFilesWithAvailableStatus.reduce(
+          (message: string, file: File) => `${message}- ${file.device}: ${file.path}\n`,
+          'Available files to download:\n\n',
+        ),
+      );
+    } else {
+      alert('There are no selected available files to download.');
+    }
+  };
+
+  return (
+    <header>
+      <input type="checkbox" className="select-all-checkbox" checked={isSelectAllChecked} />
+      <span className="selected-files-count">
+        {selectedFiles.size === 0 ? 'None Selected' : `Selected ${selectedFiles.size}`}
+      </span>
+      <button onClick={handleDownloadFiles}>
+        <img src={DownloadIcon} alt="icon" />
+        Download Selected
+      </button>
+    </header>
+  );
+}
+
+export default FileTableToolbar;
